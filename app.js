4eb495152f6642b7ec29d9f367dcb87acef88b29
lakshya-sinha <like.lakshyasinha@gmail.com>
2025-10-13 14:40:25 +0530

    bug fix

--- app.js
+++ app.js
@@ -337,7 +337,7 @@ app.post("/api/v1/saveTrialStudents", async (req, res) => {
       });
     }
 
-    res.redirect(req.originalUrl.replace("/api/v1/saveTrialStudents", "/"));
+  es.redirect(req.headers.origininalUrl);
   } catch (error) {
     console.error(error);
     res.status(500).send("Error saving trial student or sending mail");
@@ -376,7 +376,7 @@ app.post("/api/v1/saveContactDetails", async (req, res) => {
       });
     }
 
-     res.redirect(req.originalUrl.replace("/api/v1/saveTrialStudents", "/"));
+     res.redirect(req.headers.origininalUrl);
   } catch (error) {
     console.error(error);
     res.status(500).send("Error saving contact details or sending mail");
